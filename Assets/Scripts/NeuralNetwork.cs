be872cdccba974a6d440c5d70f4e2354d3ab53f0
Jacky Yang <jackyyang267@gmail.com>
2020-03-05 23:59:56 -0500

    Added superficial code

---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Try this page for howto
+/// https://towardsdatascience.com/first-neural-network-for-beginners-explained-with-code-4cfd37e06eaf
+/// </summary>
+public class NeuralNetwork : MonoBehaviour
+{
+    public struct Neuron
+    {
+        Vector3 weights;
+        float bias; // Do we even use this lmao
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
