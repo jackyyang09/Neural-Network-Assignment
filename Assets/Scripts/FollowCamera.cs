517c75c0c1bfbf5bc7512c7d830930551d8de450
Jacky Yang <jackyyang267@gmail.com>
2020-03-07 15:20:25 -0500

    Added basic track generation, a car follow cam, and assets for car model

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3F;
+    public float rotTime = 0.25f;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotVel = Vector3.zero;
+
+    void Update()
+    {
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+        transform.eulerAngles = Vector3.SmoothDamp(transform.eulerAngles, target.eulerAngles, ref rotVel, rotTime);
+    }
+}
